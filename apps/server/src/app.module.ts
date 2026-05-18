5a9265a6b5450c70c455743bdc2ef0711b8bf36e
Alejandro langarica <alelangaapa@gmail.com>
2026-05-18 22:05:24 +0200

    Register auth module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
